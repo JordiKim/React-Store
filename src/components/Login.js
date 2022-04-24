4fe13e6991e0742e078447bafdc1dafe326d680a
jordi <jordikimj@gmail.com>
2022-04-24 15:43:37 +0800

    first react-project creat

---
+++ src/components/Login.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Login extends React.Component {
+    render() {
+        return <p>Login Component</p>; // 在js寫html的寫法稱為JSX，需要使用Babel轉碼器
+    }
+}
+
+export default Login;
